0de1b4a86595d72e77f2840f1a007f993d17a8bf
ysayonnar <ysayonnar@gmail.com>
2024-12-28 18:09:49 +0300

    bubble sort

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	sortingalgos "algos/sortingAlgos"
+	"fmt"
+)
 
+func main() {
+	arr := []int{1, 0, -1, -2, -3, 123, 2, 99}
+	sortingalgos.BubbleSort(arr)
+	fmt.Println(arr)
 }
---
+++ sortingAlgos/bubbleSort.go
@@ -0,0 +1,13 @@
+package sortingalgos
+
+func BubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				temp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+		}
+	}
+}
