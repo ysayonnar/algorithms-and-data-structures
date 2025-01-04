922dd34f7d2632599bfb36226f3ee867bdc8cf6e
ysayonnar <ysayonnar@gmail.com>
2025-01-04 13:19:52 +0300

    leetocode 2215

---
+++ leetcode/2215.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func pivotIndex(nums []int) int {
+	prefixSum := make([]int, len(nums))
+	suffixSum := make([]int, len(nums))
+	prefixSum[0] = nums[0]
+	suffixSum[len(nums)-1] = nums[len(nums)-1]
+
+	for i := 1; i < len(nums); i++ {
+		prefixSum[i] = prefixSum[i-1] + nums[i]
+		suffixSum[len(nums)-i-1] = suffixSum[len(nums)-i] + nums[len(nums)-i-1]
+	}
+	for i := 0; i < len(nums); i++ {
+		if prefixSum[i] == suffixSum[i] {
+			return i
+		}
+	}
+
+	return -1
+}
--- main.go
+++ main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	sortingalgos "algos/sortingAlgos"
-	"fmt"
-)
-
 func main() {
-	arr := []int{1, 0, -1, -2, -3, 123, 2, 99}
-	fmt.Println("Before: ", arr)
-	sortingalgos.SelectionSort(arr)
-	fmt.Println("After: ", arr)
-
-	fmt.Println("Is sorted: ", sortingalgos.IsSorted(arr))
 }
