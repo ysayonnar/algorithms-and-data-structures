311b858580d645e514e98744d4747bac9a64efd4
ysayonnar <ysayonnar@gmail.com>
2025-01-06 20:50:35 +0300

    leetcode 394

---
+++ leetcode/394.go
@@ -0,0 +1,50 @@
+package leetcode
+
+import (
+	"strconv"
+)
+
+func isNumber(s byte) bool {
+	if s == '1' || s == '2' || s == '3' || s == '4' || s == '5' || s == '6' || s == '7' || s == '8' || s == '9' || s == '0' {
+		return true
+	}
+	return false
+}
+
+func decodeString(s string) string {
+	result := []byte{}
+
+	leftIdx := 0
+	rightIdx := len(s) - 1
+	timesBytes := []byte{}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '[' {
+			leftIdx = i
+			boundsCounter := 0
+			for j := i + 1; j < len(s); j++ {
+				if s[j] == '[' {
+					boundsCounter++
+				} else if s[j] == ']' && boundsCounter == 0 {
+					rightIdx = j
+					j = len(s)
+				} else if s[j] == ']' {
+					boundsCounter--
+				}
+			}
+			times, _ := strconv.Atoi(string(timesBytes))
+			timesBytes = []byte{}
+			temp := []byte(decodeString(s[leftIdx+1 : rightIdx]))
+			for i := 0; i < times; i++ {
+				result = append(result, temp...)
+			}
+			i = rightIdx
+		} else if isNumber(s[i]) {
+			timesBytes = append(timesBytes, s[i])
+		} else {
+			result = append(result, s[i])
+		}
+	}
+
+	return string(result)
+}
--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"algos/leetcode"
-	"fmt"
-)
-
 func main() {
-	fmt.Println(leetcode.EqualPairs([][]int{
-		{3, 1, 2, 2},
-		{1, 4, 4, 5},
-		{2, 4, 2, 2},
-		{2, 4, 2, 2},
-	}))
+
 }
