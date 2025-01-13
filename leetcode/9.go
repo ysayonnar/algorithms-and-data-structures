6f5caaae02fe6b3d90da31982cad56691935ef9c
ysayonnar <ysayonnar@gmail.com>
2025-01-13 20:48:23 +0300

    leetcode 9

---
+++ leetcode/9.go
@@ -0,0 +1,39 @@
+package leetcode
+
+func isPalindrome(x int) bool {
+	//НОРМАЛЬНОЕ РЕШЕНИЕ
+	if x < 0 {
+		return false
+	} else if x < 10 {
+		return true
+	}
+	digits := []int{}
+	for x >= 10 {
+		remainder := x % 10
+		digits = append(digits, remainder)
+		x /= 10
+	}
+	digits = append(digits, x)
+
+	i, j := 0, len(digits)-1
+	for i <= j {
+		if digits[i] != digits[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+
+	//ПИДОРСКОЕ РЕШЕНИЕ!!!!
+	// str := strconv.Itoa(x)
+	// i, j := 0, len(str)-1
+	// for i <= j {
+	// 	if str[i] != str[j] {
+	// 		return false
+	// 	}
+	// 	i++
+	// 	j--
+	// }
+	// return true
+}
--- main.go
+++ main.go
@@ -1,26 +1,4 @@
 package main
 
-import (
-	"algos/dataStructures"
-	"fmt"
-)
-
 func main() {
-	tree := dataStructures.Tree{}
-	tree.Insert(50)
-	tree.Insert(25)
-	tree.Insert(20)
-	tree.Insert(15)
-	tree.Insert(17)
-	tree.Insert(10)
-	tree.Insert(5)
-	tree.Insert(-1)
-	tree.Insert(75)
-	tree.Insert(80)
-	tree.Insert(90)
-	tree.Insert(95)
-	fmt.Println("Search 5: ", tree.Search(5))
-	// tree.Delete(5)
-	fmt.Println("Search 5: ", tree.Search(5))
-	tree.PrintTreeStructure()
 }
