3d2fa13fac78795683ba94328051cfbafbe20166
ysayonnar <ysayonnar@gmail.com>
2025-01-28 17:24:40 +0300

    leetcode 374

---
+++ leetcode/374.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func guess(num int) int //leetcode implements
+
+func guessNumber(n int) int {
+	left := 1
+	right := n
+
+	for left <= right {
+		mid := left + (right-left)/2
+		res := guess(mid)
+		if res == 0 {
+			return mid
+		} else if res == -1 {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return 1
+}
